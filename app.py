146002e833002cdbe13aef30e393bdf627c3c000
Ryan Wong <renfeirw@andrew.cmu.edu>
2026-08-31 09:00:09 -0400

    Week 1 Commit 2 - feat: build initial CMU course advisor interface

--- app.py
+++ app.py
@@ -4,9 +4,10 @@ from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 from pydantic import BaseModel
 from Engine.planning import (
-    generate_semester_path,
+    generate_multiple_paths,
     get_path_explanation
 )
+from Engine.workload import calculate_semester_load
 
 
 app = FastAPI()
@@ -19,6 +20,7 @@ def load_json(path):
 
 courses = load_json("data/courses.json")
 requirements = load_json("data/requirements.json")
+course_metrics = load_json("data/course_metrics.json")
 
 
 class PlanRequest(BaseModel):
@@ -27,19 +29,35 @@ class PlanRequest(BaseModel):
     start_semester: str = "spring"
     max_units: int = 24
 
+def attach_workload_to_path(path_result):
+    for semester in path_result["path"]:
+
+        workload = calculate_semester_load(
+            semester["courses"],
+            course_metrics
+        )
+
+        semester["workload"] = workload
+
+    return path_result
 
 @app.post("/api/plan")
 def create_plan(request: PlanRequest):
 
-    result = generate_semester_path(
-        completed_courses=request.completed_courses,
-        courses=courses,
-        program_id=request.goal,
-        requirements=requirements,
-        start_semester=request.start_semester,
-        num_semesters=4,
-        max_units=request.max_units
-    )
+    result = generate_multiple_paths(
+    completed_courses=request.completed_courses,
+    courses=courses,
+    program_id=request.goal,
+    requirements=requirements,
+    start_semester=request.start_semester
+)
+    result["fastest"] = attach_workload_to_path(
+        result["fastest"]
+            )
+
+    result["lower_workload"] = attach_workload_to_path(
+        result["lower_workload"]
+        )
 
     explanation = get_path_explanation(
         completed_courses=request.completed_courses,
@@ -48,9 +66,11 @@ def create_plan(request: PlanRequest):
         requirements=requirements
     )
 
-    result["explanation"] = explanation
-
-    return result
+    return {
+        "fastest": result["fastest"],
+        "lower_workload": result["lower_workload"],
+        "explanation": explanation
+    }
 
 
 app.mount(
